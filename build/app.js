631a51fbf88870d40470a7230657240647a1db04
Srinivas Reddy Katta <srinivas.reddy.katta@toyota.com>
2018-09-05 11:21:58 -0500

    read me file and missing build folder created.

---
+++ build/app.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var path_1 = __importDefault(require("path"));
+var cookie_parser_1 = __importDefault(require("cookie-parser"));
+var morgan_1 = __importDefault(require("morgan"));
+var index_1 = __importDefault(require("./routes/index"));
+var users_1 = __importDefault(require("./routes/users"));
+var books_1 = __importDefault(require("./routes/books"));
+var app = express_1.default();
+app.use(morgan_1.default('dev'));
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use(cookie_parser_1.default());
+app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
+app.use('/', index_1.default);
+app.use('/users', users_1.default);
+app.use('/books', books_1.default);
+module.exports = app;
